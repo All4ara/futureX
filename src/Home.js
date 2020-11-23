351b50ffadbede6f4949ac697689ee3a63b96d99
Ariel Elazarov <ariel.elazarov@yahoo.com>
2020-11-23 11:14:22 -0500

    first commit

---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from './Navbar.js'
+
+const Home = () => {
+    
+    
+    
+    
+    
+    
+    
+    return(
+        <>
+        <Navbar/>
+
+        <div className="container-fluid padding my-4">
+            <div className="row text-center padding col-12">
+                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                    <img src="https://ichef.bbci.co.uk/news/1024/branded_news/7727/production/_103330503_musk3.jpg" alt="the-Goat" style={{width:'100%'}}/>
+                </div>
+                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                    <h1>Invest in a Mission</h1>
+                    <p>Join Scientists, Companies and Governmants in discovering the sectets of the solar system by investing in a FutureX Mission.</p>
+                    <button>Discover Space</button>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+
+
+
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-md navbar-light bg-dark sticky-top">
+                <div className="container-fluid">
+                    <Link to="/"><span className="navbar-name">FutureX</span></Link>
+                    
+                    <button className="navbar-toggler" type="button" datatoggle="collapse" datatarget="#navbarResponsive">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ml-auto text-right">
+                            <li className="nav-item ">
+                                <button className="nav-link " href="#1">Discover Space</button>
+                            </li>
+                            <li className="nav-item">
+                                <button className="nav-link" href="google.com">About FutureX</button>
+                            </li>
+                            <li className="nav-item">
+                                <button className="nav-link" href="google.com">LogIn</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+
+export default Navbar;
