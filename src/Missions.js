5f2d2badd309299f75c870efd62f3615589b317d
Darys Bada <badadarys@gmail.com>
2020-11-25 12:24:42 -0500

    Slight Txt Change

--- src/Missions.js
+++ src/Missions.js
@@ -13,8 +13,9 @@ const Missions = (props) => {
             <div className="mission1 container-fluid padding border-bottom" style={{height: '35rem'}}>
                 <div className="row text-left padding my-5 col-12">
                     <div className="missionTxt col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                        <h1 className="mt-5">Mission 1</h1>
-                        <p>Join Scientists, Companies and Governmants in discovering the sectets of the solar system by investing in a FutureX Mission.</p>
+                        <h1 className="mt-5">Mission</h1>
+                        <h1>Space Station</h1>
+                        <p>Designed from the beginning to transport people, SpaceX’s Dragon spacecraft is capable of carrying both NASA astronauts and private passengers to destinations in low-Earth orbit, the Moon and beyond.</p>
                     </div>
                     <div className="missionBttnR col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
                         <Link to="/missions/m1"><button className="missionB px-5 py-3 mt-5">See Details</button></Link>
